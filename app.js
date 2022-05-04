cb9efd8520bcac787af7c65a98056b4600720d9c
shuvomoy <shuvo151dey@gmail.com>
2022-05-05 01:25:47 +0530

    first view added

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
-const http = require('http');
-// const app = require('express');
-
-const server = http.createServer((req,res)=>{
-    res.setHeader('Content-Type','text/plain');
-    res.write("Server reached");
-    res.end()
-})
+// const http = require('http');
+const express = require('express');
+const app = express();
+// const server = http.createServer((req,res)=>{
+//     res.setHeader('Content-Type','text/plain');
+//     res.write("Server reached");
+//     res.end()
+// })
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://Shuvo_15:BEDwkRf37O2EIqeD@cluster0.scyfh.mongodb.net/self_projects?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-server.listen(8000, 'localhost', ()=>{
+app.set('view engine','ejs');
+app.use("/styles", express.static(__dirname + '/views/styles'));
+app.use("/assets",express.static(__dirname+'/views/assets'));
+app.listen(8000, (error)=>{
+    if(error) throw error;
+    console.log("connection successful!!!");
     client.connect(err => {
       const collection = client.db("self_projects").collection("profiler");
       // perform actions on the collection object
@@ -19,6 +24,11 @@ server.listen(8000, 'localhost', ()=>{
       client.close();
     });
 
+})
+
+app.get('/', (req,res) => {
+    // res.sendFile('./views/index.html', {root: __dirname});
+    res.render('index');
 })
 
 
